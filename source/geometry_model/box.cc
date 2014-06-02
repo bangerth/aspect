2395400cdad524b4ac207c54ca8d462e28b6c436
Wolfgang Bangerth <bangerth@math.tamu.edu>
2014-06-02 10:21:29 -0500

    Let the 'box' geometry describe its boundary pieces.

--- source/geometry_model/box.cc
+++ source/geometry_model/box.cc
@@ -78,6 +78,40 @@ namespace aspect
       return s;
     }
 
+
+
+    template <int dim>
+    std::map<std::string,types::boundary_id>
+    Box<dim>::
+    get_symbolic_boundary_names_map () const
+    {
+      switch (dim)
+      {
+      case 2:
+        {
+          static const std::pair<std::string,types::boundary_id> mapping[]
+          = { {"left", 0}, {"right", 1}, {"bottom", 2}, {"top", 3} };
+
+          return std::map<std::string,types::boundary_id> (&mapping[0],
+                                                           &mapping[sizeof(mapping)/sizeof(mapping[0])]);
+        }
+
+      case 3:
+        {
+          static const std::pair<std::string,types::boundary_id> mapping[]
+          = { {"left", 0}, {"right", 1}, {"front", 2}, {"back", 3}, {"bottom", 4}, {"top", 5} };
+
+          return std::map<std::string,types::boundary_id> (&mapping[0],
+                                                           &mapping[sizeof(mapping)/sizeof(mapping[0])]);
+        }
+      }
+
+      Assert (false, ExcNotImplemented());
+      return std::map<std::string,types::boundary_id>();
+    }
+
+
+
     template <int dim>
     std::set< std::pair< std::pair<types::boundary_id, types::boundary_id>, unsigned int> >
     Box<dim>::
@@ -248,7 +282,8 @@ namespace aspect
                                    "2*dim sides as follows: in 2d, boundary indicators 0 through 3 "
                                    "denote the left, right, bottom and top boundaries; in 3d, boundary "
                                    "indicators 0 through 5 indicate left, right, front, back, bottom "
-                                   "and top boundaries. See also the documentation of the deal.II class "
-                                   "``GeometryInfo''.")
+                                   "and top boundaries (see also the documentation of the deal.II class "
+                                   "``GeometryInfo''). You can also use symbolic names ``left'', ``right'',"
+                                   "etc., to refer to these boundaries.")
   }
 }
